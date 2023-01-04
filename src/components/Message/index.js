8e4f60532e8fa61a6add414f6f05c7616082a7d1
root <sanyambhagyasree09@gmail.com>
2023-01-04 16:28:01 +0000

    login-App

---
+++ src/components/Message/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+const Message = props => {
+  const {abc} = props
+
+  const text = abc ? 'Welcome User' : 'Please Login'
+  return <h1 className="msg">{text}</h1>
+}
+
+export default Message
